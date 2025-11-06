178dcc837bf8cc9e5205b01c8971389fa70d4592
BishnuThapa10 <bishnuthapaofkalika@gmail.com>
2025-11-06 07:52:57 +0545

    setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from './components/ui/button.jsx'
+
+export default function App() {
+  return (
+    <div>
+      <div className="p-6">
+      <Button>Click me</Button>
+    </div>
+    </div>
+  )
+}
