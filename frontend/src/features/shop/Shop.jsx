215933b040b5d9504250c34a33611a246f671b81
BishnuThapa10 <bishnuthapaofkalika@gmail.com>
2025-11-07 20:19:04 +0545

    shop Start

--- frontend/src/features/shop/Shop.jsx
+++ frontend/src/features/shop/Shop.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import ShopHeroSection from './ShopHeroSection.jsx'
+import FurnitureList from './FurnitureList.jsx'
 
 export default function Shop() {
   return (
     <div>
-      
+      <ShopHeroSection/>
+      <FurnitureList/>
     </div>
   )
 }
