5213b594eb85685c51952d09b6f2e53977fdf084
BishnuThapa10 <bishnuthapaofkalika@gmail.com>
2025-11-30 15:00:44 +0545

    hosting url

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,10 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.use(cors({
-  origin:['http://localhost:5173',],
+  origin: [
+    'http://localhost:5173',
+    'https://e-commerce-seven-tau-74.vercel.app'
+  ],
   credentials: true,
 }));
 
@@ -65,5 +68,5 @@ connectDB().then(() => {
     console.log(`Server running on port ${port}`);
   });
 }).catch((err) => {
-    console.log(err)
-  });
+  console.log(err)
+});
